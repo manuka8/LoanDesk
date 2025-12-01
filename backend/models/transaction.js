1dd47ee18165b8ff008ce69cd14b2a9332b3dad1
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-12-01 21:28:22 +0530

    Adding transaction model

--- backend/models/transaction.js
+++ backend/models/transaction.js
@@ -5,15 +5,18 @@ const Loan = require('./loan');
 
 const Transaction = sequelize.define('Transaction', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  action: { type: DataTypes.STRING(100) },
+
+  action: { type: DataTypes.STRING(100), allowNull: false },
   amount: { type: DataTypes.DECIMAL(10,2) },
+
   timestamp: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
 }, {
   tableName: 'transactions',
   timestamps: false,
 });
 
-Transaction.belongsTo(User, { foreignKey: 'userId' });
-Transaction.belongsTo(Loan, { foreignKey: 'loanId' });
+// Associations
+Transaction.belongsTo(User, { foreignKey: 'userId', onDelete: 'SET NULL' });
+Transaction.belongsTo(Loan, { foreignKey: 'loanId', onDelete: 'CASCADE' });
 
 module.exports = Transaction;
